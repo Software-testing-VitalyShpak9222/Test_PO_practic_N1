523f2270e2f10f8e282c64b9f2a87ac98b365764
VitalyShpak9222 <112765255+VitalyShpak9222@users.noreply.github.com>
2025-03-18 12:52:32 +0300

    Update LoginServer.java

--- src/main/java/org/example/LoginServer.java
+++ src/main/java/org/example/LoginServer.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class LoginServer {
     public boolean authenticate(String username, String password) {
-        // Для простоты, используем фиксированные данные для аутентификации
         return "user".equals(username) && "password".equals(password);
     }
 }
